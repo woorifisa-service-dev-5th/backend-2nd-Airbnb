86275371c4c0aad1ee1da92ff82dc2a436c32b97
TaekkiMin <tktwin00@gmail.com>
2025-08-25 10:36:40 +0900

    refactor: like(좋아요) 필드 추가

--- src/main/java/model/TeamAccommodation.java
+++ src/main/java/model/TeamAccommodation.java
@@ -3,11 +3,13 @@ package model;
 public class TeamAccommodation {
 	int teamID;
 	int accomID;
+	int like;
 
-	public TeamAccommodation(int teamID, int accomID) {
+	public TeamAccommodation(int teamID, int accomID, int like) {
 		super();
 		this.teamID = teamID;
 		this.accomID = accomID;
+		this.like = like;
 	}
 
 	public int getTeamID() {
@@ -16,6 +18,14 @@ public class TeamAccommodation {
 
 	public void setTeamID(int teamID) {
 		this.teamID = teamID;
+	}
+
+	public int getLike() {
+		return like;
+	}
+
+	public void setLike(int like) {
+		this.like = like;
 	}
 
 	public int getAccomID() {
